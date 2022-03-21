6dbbe7dae73de53eab7c9b2e5ed63933808500e6
ceparman <info@ngsanalytics.com>
2022-03-21 18:58:16 +0000

    update

--- ROIapp.R
+++ ROIapp.R
@@ -6,12 +6,15 @@ library(shiny)
 library(readr)
 library(dplyr)
 library(tidyr)
+library(shinyWidgets)
 
 
 
 
 ## Load Julian function
 source("ROIhelpers.R")
+
+library(shinydashboard)
 
 
 ## Set timeout length to 1 hour
@@ -35,100 +38,181 @@ inactivity <- "function idleTimer() {
 }
 idleTimer();"
 
+### color
 
-ui <- fluidPage(
-  
-  ## Call timeout 
-  tags$script(inactivity),
+clorox_dk_blue = "#0F4C92"
 
-  
-  
-  ## Title
-  titlePanel("Cost Benefit to Adding Cleaning Time"),
-  
-  
-  ## Clorox Theme
-  theme = bs_theme(bg = "#215daa", fg = "white", primary = "#ffcd00",
-                   base_font = font_google("Open Sans"),
-                   code_font = font_google("Open Sans")),
-  
-     sidebarLayout(
-       sidebarPanel(
-         
-      numericInput(
-        inputId = "V1",
-        label = "Cost (in $) to treat a hospital-acquired C. diff infection",
-        value = 12313
-      ),
+clorox_lt_blue = "#008DD1"
+
+clorox_yellow = "#FFD92A"
+
       
-      numericInput(
-        inputId = "PRnorm",
-        label = "Base risk (in %) of acquiring a C. diff infection",
-        value = 11,
-      ),
+ui <- fluidPage(
+    tags$script(inactivity),
+   
+    useShinydashboard(),
+     
+     ## Clorox Theme
+    # theme = bs_theme(
+    #   bg = "#215daa",
+    #   fg = "white",
+    #   primary = "#ffcd00",
+    #   base_font = font_google("Open Sans"),
+    #   code_font = font_google("Open Sans")
+    # ),
+    
+    ## Clorox Them 
+    theme = bs_theme(bg =   "white" ,
+                     fg = clorox_dk_blue, 
+                     primary = "#B29612",
+                     secondary = clorox_lt_blue , 
+                     info=clorox_yellow,
+                     base_font = font_google("Lato"),
+                     code_font = font_google("Lato"),
+                     heading_font = font_google("Tinos"),
+                   font_scale = 1.
+          ),
+      
+               titlePanel(windowTitle = "CloroxPro" ,
+                          fluidRow(
+                            
+                                     column(2,align="left",
+                                            img( src="clorox.JPG",height="100px")
+                                            ),
+                                     column(8,align="center",
+                                     h1("Cost Benefit to Adding Cleaning Time", align = "center")
+                                      ),
+                          )            
+                          ),
+    hr(style= paste("border-color:",clorox_yellow)),
+               sidebarLayout(
+                 
+                 sidebarPanel(width = 4,
+                   
+                    fluidRow(
+                      
+                              column(6,
+                                    verticalLayout( 
+                                   #   numericInput(
+                                  #      inputId = "V1",
+                                  #      label = "Cost (in $) to treat a hospital-acquired C. diff infection",
+                                  #      value = 12313
+                                  #    ),
+                                      
+                                      numericInputIcon(
+                                        inputId = "V1",
+                                        label = "Cost (in $) to treat a hospital-acquired C. diff infection",
+                                        value = 12313,
+                                        icon = list(icon("dollar-sign"),NULL)
+                                      ),
+                                      
+                                      
+                                      numericInput(
+                                        inputId = "PRnorm",
+                                        label = "Base risk (in %) of acquiring a C. diff infection",
+                                        value = 11,
+                                      ),
+                                      
+                                      numericInput(
+                                        inputId = "PRintv",
+                                        label = "Risk (in %) of acquiring a C. diff infection after enhanced cleaning and disinfecting",
+                                        value = 4.6,
+                                      ),    numericInput(
+                                        inputId = "Time",
+                                        label = "Time (in minutes) to perform enhanced disinfection",
+                                        value = 15,
+                                      ),
+                                      
+                                      
+                                      radioGroupButtons(
+                                        inputId = "N_enhanced_peryear",
+                                        label = "Number of times each year you expect to perform an enhanced clean with an electrostatic spray device",
+                                        choiceNames = c("daily","weekly","bi-weekly","monthly"),
+                                        selected=52,
+                                        choiceValues = c(365,52,21,12)
+                                      )
+                                      
+                                      )  
+                                     ),
+                              column(6,
+                                     verticalLayout(
+                                     numericInput(
+                                       inputId = "Housekeeper_wage",
+                                       label = "Housekeeper wage per hour (in $)",
+                                       value = 12.89,
+                                     ),
+                                     
+                                     numericInput(
+                                       inputId = "Cleaning_standard",
+                                       label = "Cost of supplies per standard room clean (in $)",
+                                       value = 0.38,
+                                     ),
+                                     
+                                     numericInput(
+                                       inputId = "Cleaning_enhanced",
+                                       label = "Cost of supplies per enhanced room clean (in $)",
+                                       value = 0.81,
+                                     ),
+                                     numericInput(
+                                       inputId = "Hospital_rent",
+                                       label = "Cost per day (in $) for a patient to stay in hospital room",
+                                       value = 2502,
+                                     ),
+                                     
+                                     numericInput(
+                                       inputId = "Device_cost",
+                                       label = "Cost of electrostatic spray device (in $)",
+                                       value = 4500,
+                                       )
+                                    ) 
+                                     
+                              )  
+                    ) #end Row       
+                              
+                
+                 ),
+                 mainPanel(
+                  
+                   fluidRow(
+                    column(6,
+                           wellPanel(
+                            
+                             
+                             uiOutput("results")
+                           )
+                    ),
+                    
+                    column(6,
+                           wellPanel(
+                             h3('A Title'),
+                             tags$text("Some text describing the project")
+                            
+                            
+                    )
+                    )
+                   )
+                   
+                 
+               )
+          
+                          
  
-       numericInput(
-         inputId = "PRintv",
-         label = "Risk (in %) of acquiring a C. diff infection after enhanced cleaning and disinfecting",
-         value = 4.6,
-      ),
-  
-        numericInput(
-          inputId = "Time",
-          label = "Time (in minutes) to perform enhanced disinfection",
-          value = 15,
-      ),
-  
-        numericInput(
-          inputId = "Hospital_rent",
-          label = "Cost per day (in $) for a patient to stay in hospital room",
-          value = 2502,
-        ),
-  
-        numericInput(
-          inputId = "Housekeeper_wage",
-          label = "Housekeeper wage per hour (in $)",
-          value = 12.89,
-        ),
-  
-        numericInput(
-          inputId = "Cleaning_standard",
-          label = "Cost of supplies per standard room clean (in $)",
-          value = 0.38,
-        ),
-  
-        numericInput(
-          inputId = "Cleaning_enhanced",
-          label = "Cost of supplies per enhanced room clean (in $)",
-          value = 0.81,
-        ),
-  
-        numericInput(
-          inputId = "Device_cost",
-          label = "Cost of electrostatic spray device (in $)",
-          value = 4500,
-        ),
-  
-        numericInput(
-          inputId = "N_enhanced_peryear",
-          label = "Number of times each year you expect to perform an enhanced clean with an electrostatic spray device",
-          value = 52,
-        ),
-
-       ),
-  
-    mainPanel(htmlOutput("savings"),
-              htmlOutput("risk"))
-             
-     )
-  
+),
+hr(style= paste("border-color:",clorox_yellow))
 )
+  
+
+
+      
+      
+        
+
 
 server <- function(input, output, session){
+ 
   
-  
-  output$savings <- renderUI({
-    
+  output$results <- renderUI({
+ 
     V1 <- input$V1
     PRnorm <- input$PRnorm
     PRintv <- input$PRintv
@@ -138,7 +222,7 @@ server <- function(input, output, session){
     Cleaning_standard <- input$Cleaning_standard
     Cleaning_enhanced <- input$Cleaning_enhanced
     Device_cost <- input$Device_cost
-    N_enhanced_peryear <- input$N_enhanced_peryear
+    N_enhanced_peryear <- as.numeric( input$N_enhanced_peryear )
     
     result <- ROI_calculator(V1 = V1, PRnorm = PRnorm, PRintv = PRintv, Time = Time, Hospital_rent = Hospital_rent, 
                              Housekeeper_wage = Housekeeper_wage, Cleaning_standard = Cleaning_standard, Cleaning_enhanced = Cleaning_enhanced,
@@ -146,10 +230,17 @@ server <- function(input, output, session){
     
     str1 <- result[[1]]
     str2 <- result[[2]]
+    v1 <- result[[3]]
+    v2 <- result[[4]]
     
-    
-    HTML(paste(str1, str2, sep = '<br/>'))
-    
+    tagList(
+      valueBox( paste("$",v1),subtitle =paste("Cost Reduction /n", str1),width=12,
+               color="green"
+           ) ,
+      valueBox( paste(v2,"%"), subtitle = paste("Infection Reduction", str2), width=12,
+     color= "blue"
+          )
+    )
   })
   
 
--- ROIhelpers.R
+++ ROIhelpers.R
@@ -13,6 +13,7 @@ library(ggforce)
 library(shiny)
 library(readr)
 library(bslib)
+library(stringr)
 
 ## Function for the Shiny app to use
 
@@ -41,10 +42,10 @@ ROI_calculator <- function(V1 = 12313, PRnorm = 11, PRintv = 4.6, Time = 15, Hos
   
   
   ## Outputs
-  Ob_output <- paste0("Estimate Cost Savings Per Year of Adding Enhanced Cleaning and Disinfection: $", Ob)
-  PRinc_output <- paste0("Probability risk reduction of in-room transmission: ", PRinc_print, "%")
+  Ob_output <- "Estimate Cost Savings Per Year of Adding Enhanced Cleaning and Disinfection."# $", Ob)
+  PRinc_output <- "Probability risk reduction of in-room transmission." # , PRinc_print, "%")
   
-  result <- as.list(c(Ob_output, PRinc_output))
+  result <- as.list(c(Ob_output, PRinc_output,prettyNum(Ob ,scientific = FALSE, big.mark = ","),PRinc_print))
   
   return(result)
   
@@ -52,3 +53,36 @@ ROI_calculator <- function(V1 = 12313, PRnorm = 11, PRintv = 4.6, Time = 15, Hos
 }
 
 ROI_calculator()
+
+
+customInfoBox <- function(title, value = NULL, subtitle = NULL,
+                    icon = shiny::icon("bar-chart"), color = "aqua", width = 4, href = NULL,
+                    fill = FALSE) {
+  
+#  validateColor(color)
+  shinydashboard:::tagAssert(icon, type = "i")
+  
+  colorClass <- paste0("bg-", color)
+  
+  boxContent <- div(
+    class = "info-box",
+    class = if (fill) colorClass,
+    span(
+      class = "info-box-icon",
+      class = if (!fill) colorClass,
+      icon
+    ),
+    div(class = "info-box-content",
+        span(class = "info-box-text", title),
+        if (!is.null(value)) span(class = "info-box-number", value),
+        if (!is.null(subtitle)) p(subtitle)
+    )
+  )
+  
+  if (!is.null(href))
+    boxContent <- a(href = href, boxContent)
+  
+  div(class = if (!is.null(width)) paste0("col-sm-", width),
+      boxContent
+  )
+}
